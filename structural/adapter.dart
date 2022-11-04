e1816e1a43ed36ecb633a81a10ad0fb4414d98b8
ELEliseev <eliseevlifter@gmail.com>
2022-11-04 19:55:38 +0300

    add adapter

---
+++ structural/adapter.dart
@@ -0,0 +1,31 @@
+class UpperCase {
+  String send(String data) {
+    return data.toUpperCase();
+  }
+}
+
+class SendData {
+  String push(String data) {
+    data += '\n';
+    data += 'this lower';
+    return data.toLowerCase();
+  }
+}
+
+class Adapter extends UpperCase {
+  SendData lower;
+  Adapter(this.lower);
+
+  @override
+  String send(String data) {
+    String lower = this.lower.push(data);
+    return super.send(lower);
+  }
+}
+
+void main(List<String> args) {
+  SendData lowerCase = SendData();
+
+  String transformed = Adapter(lowerCase).send('data');
+  print(transformed);
+}
