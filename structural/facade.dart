7acf4bea1c24ee2f5b829b7e0a3d08696d6e1432
ELEliseev <eliseevlifter@gmail.com>
2022-11-06 11:17:38 +0300

    add facade

---
+++ structural/facade.dart
@@ -0,0 +1,76 @@
+// Фасад — это структурный паттерн проектирования,
+//  который предоставляет простой интерфейс
+//  к сложной системе классов,
+//   библиотеке или фреймворку.
+
+class Radio {
+  String play() {
+    return 'radio';
+  }
+}
+
+class Mp3 {
+  String play() {
+    return 'mp3';
+  }
+}
+
+class Bluetooth {
+  String play() {
+    return 'bluetooth';
+  }
+}
+
+class Avi {
+  String play() {
+    return 'avi';
+  }
+}
+
+enum MusicType { Avi, Mp3, Radio, Bloetooth }
+
+class Player {
+  final Avi avi;
+  final Bluetooth bluetooth;
+  final Mp3 mp3;
+  final Radio radio;
+
+  Player(
+      {required this.radio,
+      required this.mp3,
+      required this.avi,
+      required this.bluetooth});
+
+  String play(MusicType type) {
+    switch (type) {
+      case MusicType.Avi:
+        return avi.play();
+      case MusicType.Mp3:
+        return mp3.play();
+      case MusicType.Radio:
+        return radio.play();
+      case MusicType.Bloetooth:
+        return bluetooth.play();
+      default:
+        throw Exception('this unknown format');
+    }
+  }
+}
+
+void main(List<String> args) {
+  final radio = Radio();
+  final mp3 = Mp3();
+  final avi = Avi();
+  final bluetooth = Bluetooth();
+
+  Player player =
+      Player(radio: radio, mp3: mp3, avi: avi, bluetooth: bluetooth);
+
+  if (args.isEmpty) {
+    return;
+  }
+
+  MusicType type = MusicType.values.firstWhere((type) => args[0] == type.name);
+
+  print(player.play(type));
+}
