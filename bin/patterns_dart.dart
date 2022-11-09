e8f815a8d13aad95c2906facb1e96b0fc0f9ea55
ELEliseev <eliseevlifter@gmail.com>
2022-11-09 20:48:56 +0300

    overlap in folders

---
+++ bin/patterns_dart.dart
@@ -0,0 +1,5 @@
+import 'package:patterns_dart/patterns_dart.dart' as patterns_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${patterns_dart.calculate()}!');
+}
---
+++ test/patterns_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:patterns_dart/patterns_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
